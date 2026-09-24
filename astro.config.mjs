37b07bb72005485c150c3ad9212ce3dbbfb382c8
ghulakis <55784122+ghulakis@users.noreply.github.com>
2026-09-24 16:31:04 +0200

    Build editorial blog experience: astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  site: 'https://ghulakis.github.io',
+  base: '/george-choulakis-blog',
   integrations: [mdx()],
 });
